1fcf844f9f64a0c9fe86a80722ea1572f9e71a5a
sgaron-cse <sgaron.cse@gmail.com>
2018-10-03 15:28:29 -0400

    Prepare test environment

---
+++ test/test_filestore.py
@@ -0,0 +1,55 @@
+
+def test_http():
+    """
+
+    :return:
+    """
+    pass
+
+
+def test_https():
+    """
+
+    :return:
+    """
+    pass
+
+
+def test_sftp():
+    """
+
+    :return:
+    """
+    pass
+
+
+def test_ftp():
+    """
+
+    :return:
+    """
+    pass
+
+
+def test_ftps():
+    """
+
+    :return:
+    """
+    pass
+
+
+def test_file():
+    """
+
+    :return:
+    """
+    pass
+
+
+def test_s3():
+    """
+
+    :return:
+    """
+    pass
