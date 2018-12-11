76c5e3c8d65f9589242436bd4d269dd9d39fa130
sgaron-cse <sgaron.cse@gmail.com>
2018-12-11 13:18:48 -0500

    Added workflow model

---
+++ assemblyline/odm/models/workflow.py
@@ -0,0 +1,21 @@
+from assemblyline import odm
+
+PRIORITIES = {"", "LOW", "MEDIUM", "HIGH", "CRITICAL"}
+STATUSES = {"", "MALICIOUS", "NON-MALICIOUS", "ASSESS"}
+
+
+@odm.model(index=True, store=True)
+class Workflow(odm.Model):
+    classification = odm.Classification()               # Classification of the workflow
+    creation_date = odm.Date()                          # Creation date of the workflow
+    creator = odm.Keyword()                             # UID of the creator
+    edited_by = odm.Keyword()                           # UID of the last edit user
+    hit_count = odm.Integer()                           # Number of time workflow hit
+    label = odm.List(odm.Keyword())                     # Labels applied by the workflow
+    last_edit = odm.Date()                              # Last edit date
+    last_seen = odm.Date()                              # Last hit date
+    name = odm.Keyword()                                # Name of the workflow
+    priority = odm.Enum(values=PRIORITIES, default="")  # Priority applied by the workflow
+    query = odm.Keyword()                               # Query that the workflow runs
+    status = odm.Enum(values=STATUSES, default="")      # Status applied by the workflow
+    workflow_id = odm.Keyword()                         # ID of the workflow
