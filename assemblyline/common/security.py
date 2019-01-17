567544f710bada220323671fc56a7c114d9b7e0a
sgaron-cse <sgaron.cse@gmail.com>
2019-01-17 14:28:12 -0500

    Fix otp token get in common security

--- assemblyline/common/security.py
+++ assemblyline/common/security.py
@@ -22,7 +22,7 @@ def get_hotp_token(secret, intervals_no):
     key = base64.b32decode(secret, True)
     msg = struct.pack(">Q", intervals_no)
     h = hmac.new(key, msg, hashlib.sha1).digest()
-    o = ord(h[19]) & 15
+    o = h[19] & 15
     h = (struct.unpack(">I", h[o:o+4])[0] & 0x7fffffff) % 1000000
     return h
 
