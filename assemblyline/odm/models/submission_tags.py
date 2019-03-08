f6ad930e278113bad50d0668ed8d2e2a8430d14b
sgaron-cse <sgaron.cse@gmail.com>
2019-03-08 11:06:37 -0500

    Stop indexing submission tree and tags caches

--- assemblyline/odm/models/submission_tags.py
+++ assemblyline/odm/models/submission_tags.py
@@ -3,5 +3,5 @@ from assemblyline import odm
 
 @odm.model(index=True, store=False)
 class SubmissionTags(odm.Model):
-    expiry_ts = odm.Date()  # Expiry date
-    tags = odm.Keyword()    # Tags cache
+    expiry_ts = odm.Date()        # Expiry date
+    tags = odm.Text(index=False)  # Tags cache
--- assemblyline/odm/models/submission_tree.py
+++ assemblyline/odm/models/submission_tree.py
@@ -3,5 +3,5 @@ from assemblyline import odm
 
 @odm.model(index=True, store=False)
 class SubmissionTree(odm.Model):
-    expiry_ts = odm.Date()  # Expiry date
-    tree = odm.Keyword()    # Tree cache
+    expiry_ts = odm.Date()        # Expiry date
+    tree = odm.Text(index=False)  # Tree cache
