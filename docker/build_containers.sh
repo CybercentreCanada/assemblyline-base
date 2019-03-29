1835856bbab38a137e15daeba7ae8e8ddcffe599
sgaron-cse <sgaron.cse@gmail.com>
2019-03-29 15:28:34 -0400

    Trying to get Kibana APM to work in flask

--- docker/build_containers.sh
+++ docker/build_containers.sh
@@ -6,13 +6,14 @@ fi
 
 # Build core containers
 (cd elasticsearch && docker build -t sgaroncse/elasticsearch:6.4.2 .)
+(cd apm-server && docker build -t sgaroncse/apm-server:6.4.2 .)
 (cd nginx-ssl && docker build -t sgaroncse/nginx-ssl:4.0.0 -t sgaroncse/nginx-ssl:latest .)
 (cd nginx-ssl-dev && docker build -t sgaroncse/nginx-ssl-dev:4.0.0 -t sgaroncse/nginx-ssl-dev:latest .)
 (cd riak && docker build -t sgaroncse/riak-kv:2.1.4 .)
 
 # Build default dev containers
-(cd ../.. && docker build -f alv4/docker/al_dev/Dockerfile -t sgaroncse/assemblyline_dev:latest -t sgaroncse/assemblyline_dev:4.0.1 .)
-(cd ../.. && docker build -f alv4/docker/al_dev_py2/Dockerfile -t sgaroncse/assemblyline_dev_py2:latest -t sgaroncse/assemblyline_dev_py2:4.0.1 .)
+(cd ../.. && docker build -f alv4/docker/al_dev/Dockerfile -t sgaroncse/assemblyline_dev:latest -t sgaroncse/assemblyline_dev:4.0.2 .)
+(cd ../.. && docker build -f alv4/docker/al_dev_py2/Dockerfile -t sgaroncse/assemblyline_dev_py2:latest -t sgaroncse/assemblyline_dev_py2:4.0.2 .)
 
 # Build services containers
 (cd ../.. && docker build -f alv4/docker/v3_services/v3_service_base_dev/Dockerfile -t sgaroncse/v3_service_base_dev:latest -t sgaroncse/v3_service_base_dev:3.3.2 .)
