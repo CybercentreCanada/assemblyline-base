3be119853a786946b6c9ff7d464be1a2c36d5a58
sgaron-cse <sgaron.cse@gmail.com>
2019-04-01 13:39:51 -0400

    Also push apm-server

--- docker/push_containers.sh
+++ docker/push_containers.sh
@@ -6,6 +6,7 @@ fi
 
 # Push core containers
 docker push sgaroncse/elasticsearch
+docker push sgaroncse/apm-server
 docker push sgaroncse/nginx-ssl
 docker push sgaroncse/nginx-ssl-dev
 docker push sgaroncse/riak-kv
