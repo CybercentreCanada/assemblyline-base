d0e7296454601a90f99048ed2e602134b9a7177e
sgaron-cse <sgaron.cse@gmail.com>
2019-04-11 12:34:35 -0400

    Fix pipeline

---
+++ docker/elasticsearch/set_vars.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -ex
+
+if [ $DISCOVERY_TYPE ]; then
+    grep -q 'discovery.type:' config/elasticsearch.yml || echo "discovery.type: $DISCOVERY_TYPE" >> config/elasticsearch.yml
+fi
+
+if [ $NODE_NAME ]; then
+    grep -q 'node.name:' config/elasticsearch.yml || echo "node.name: $NODE_NAME" >> config/elasticsearch.yml
+fi
+
+if [ $MASTER_NODE ]; then
+    grep -q 'cluster.initial_master_nodes:' config/elasticsearch.yml || echo "cluster.initial_master_nodes: $MASTER_NODE" >> config/elasticsearch.yml
+fi
+
+if [ $CLUSTER_NAME ]; then
+    grep -q 'cluster.name:' config/elasticsearch.yml || echo "cluster.name: $CLUSTER_NAME" >> config/elasticsearch.yml
+fi
