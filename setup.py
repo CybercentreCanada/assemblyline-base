fe0f9953cd89db2a193a4ebcd34bd5eab5272e63
Adam Douglass <douglass.cse@gmail.com>
2019-05-02 14:59:43 -0400

    Remove references to python we won't support.

--- setup.py
+++ setup.py
@@ -16,8 +16,6 @@ setup(
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
