5ad4d65d2b866dd5f73a23238b510a2f644dcc3d
sgaron-cse <sgaron.cse@gmail.com>
2019-05-16 14:13:30 -0400

    Upgraded nginx containers for x-forwarded-for instead of x-forward-for

--- docker/build_containers.sh
+++ docker/build_containers.sh
@@ -4,9 +4,9 @@
 (cd elasticsearch && docker build -t sgaroncse/elasticsearch:7.0.0 .)
 (cd apm-server && docker build -t sgaroncse/apm-server:7.0.0 .)
 (cd ../.. && docker build -f alv4/docker/metricbeat/Dockerfile -t sgaroncse/metricbeat:7.0.0 .)
-(cd nginx-ssl && docker build -t sgaroncse/nginx-ssl-bitbucket:1.15.10 .)
-(cd nginx-ssl && docker build -t sgaroncse/nginx-ssl:1.15.10-2 .)
-(cd nginx-ssl-dev && docker build -t sgaroncse/nginx-ssl-dev:1.15.10-1 .)
+(cd nginx-ssl-bitbucket && docker build -t sgaroncse/nginx-ssl-bitbucket:1.15.10-1 .)
+(cd nginx-ssl && docker build -t sgaroncse/nginx-ssl:1.15.10-3 .)
+(cd nginx-ssl-dev && docker build -t sgaroncse/nginx-ssl-dev:1.15.10-2 .)
 (cd minio && docker build -t sgaroncse/minio .)
 
 # Build default dev containers
