4e45f22612acca93a0b3c2bfbc755dc85c71155c
Adam Douglass <douglass.cse@gmail.com>
2019-05-23 13:22:28 -0400

    Pick up the build version from the environment.

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
-
+import os
 from setuptools import setup, find_packages
+
+# For development and local builds use this version number, but for real builds replace it
+# with the tag found in the environment
+package_version = "4.0.0.dev0"
+for variable_name in ['BITBUCKET_TAG']:
+    package_version = os.environ.get(variable_name, package_version)
+    package_version = package_version.lstrip('v')
 
 
 setup(
     name="assemblyline",
-    version="4.0.0.dev18",
+    version=package_version,
     description="Assemblyline (v4) automated malware analysis framework base package.",
     long_description="This package provides the base functionalities for the different Assemblyline v4 components.",
     url="https://bitbucket.org/cse-assemblyline/alv4/",
