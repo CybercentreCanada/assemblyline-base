1faca7f7a6d33dae2d05b9ac4d3d26c6f8232c86
Rushi <rushi.nathvani@165gc.onmicrosoft.com>
2019-06-11 12:03:35 -0400

    Add and compile cython files in package

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import os
 
+from Cython.Build import cythonize
 from setuptools import setup, find_packages
 
 # For development and local builds use this version number, but for real builds replace it
@@ -29,6 +30,11 @@ setup(
     ],
     keywords="assemblyline malware gc canada cse-cst cse cst cyber cccs",
     packages=find_packages(exclude=['test/*']),
+    zip_safe=False,
+    ext_modules=cythonize(["assemblyline/*/*.pyx"]),
+    setup_requires=[
+        'cython'
+    ],
     install_requires=[
         'urllib3<1.25',
         'python-baseconv',
@@ -60,7 +66,9 @@ setup(
             "*solrconfig.xml",
             "*classification.yml",
             "*.magic",
-            "*sample_rules.yar"
+            "*sample_rules.yar",
+            "*.pxd",
+            "*.pyx",
         ]
     }
 )
