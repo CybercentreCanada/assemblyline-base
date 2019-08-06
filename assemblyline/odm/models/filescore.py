1e88dffb8d1d4a18e719c264608267a436b16018
Adam Douglass <douglass.cse@gmail.com>
2019-08-06 09:55:20 -0400

    Fix comments again

--- assemblyline/odm/models/filescore.py
+++ assemblyline/odm/models/filescore.py
@@ -4,8 +4,8 @@ from assemblyline import odm
 @odm.model(index=False, store=False)
 class FileScore(odm.Model):
     psid = odm.Optional(odm.UUID())      # ID of the parent submission to the associated submission
-    expiry_ts = odm.Date(index=True)     # Expiry timestamp, (time field only applies to cache invalidation, this is for garbage collection)
+    expiry_ts = odm.Date(index=True)     # Expiry timestamp, used for garbage collection.
     score = odm.Integer()                # Maximum score for the associated submission
-    errors = odm.Integer()               # Number of errors that occured during the previous analysis
+    errors = odm.Integer()               # Number of errors that occurred during the previous analysis
     sid = odm.UUID()                     # ID of the associated submission
-    time = odm.Float()                   # Epoch time at which the FileScore entry becomes invalid
+    time = odm.Float()                   # Epoch time at which the FileScore entry was created, used for cache invalidation.
