bc0b4e659efa09fa92cd8fcc5ca193343d1c2ddb
sgaron-cse <sgaron.cse@gmail.com>
2019-10-09 15:16:01 -0400

    Update all config and docker-compose files to use authentication

--- docker/elasticsearch/setup.sh
+++ docker/elasticsearch/setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 
 until curl --silent --fail -u elastic:$ELASTIC_PASSWORD http://$ELASTIC_HOST:9200/_cluster/health
 do
@@ -9,26 +9,45 @@ done
 if [ $KIBANA_PASSWORD ]; then
     echo "Setting up kibana password..."
     curl --silent -X POST -H "Content-Type: application/json" -u elastic:$ELASTIC_PASSWORD http://$ELASTIC_HOST:9200/_security/user/kibana/_password -d "{\"password\": \"$KIBANA_PASSWORD\"}"
-    curl --silent -X PUT -u elastic:$ELASTIC_PASSWORD http://$ELASTIC_HOST:9200/_security/user/kibana/_enable
 fi
+
 if [ $APM_PASSWORD ]; then
     echo "Setting up apm password..."
-    curl --silent -X POST -H "Content-Type: application/json" -u elastic:$ELASTIC_PASSWORD http://$ELASTIC_HOST:9200/_security/user/apm_system/_password -d "{\"password\": \"$APM_PASSWORD\"}"
-    curl --silent -X PUT -u elastic:$ELASTIC_PASSWORD http://$ELASTIC_HOST:9200/_security/user/apm_system/_enable
+    curl --silent -X POST -H "Content-Type: application/json" -u elastic:$ELASTIC_PASSWORD http://$ELASTIC_HOST:9200/_security/role/apm -d "{\"cluster\":[\"manage_ilm\", \"manage_pipeline\"],\"indices\":[{\"names\":[\"apm-*\",\".ml-anomalies*\"],\"privileges\":[\"all\"],\"allow_restricted_indices\":false}],\"applications\":[],\"run_as\":[],\"metadata\":{}}"
+    curl --silent -X POST -H "Content-Type: application/json" -u elastic:$ELASTIC_PASSWORD http://$ELASTIC_HOST:9200/_security/user/apm -d "{\"password\": \"$APM_PASSWORD\",\"roles\":[\"apm_user\",\"apm_system\",\"kibana_system\",\"apm\"],\"full_name\":null,\"email\":null,\"metadata\":{}}"
 fi
-if [ $BEATS_PASSWORD ]; then
-    echo "Setting up beats password..."
-    curl --silent -X POST -H "Content-Type: application/json" -u elastic:$ELASTIC_PASSWORD http://$ELASTIC_HOST:9200/_security/user/beats_system/_password -d "{\"password\": \"$BEATS_PASSWORD\"}"
-    curl --silent -X POST -H "Content-Type: application/json" -u elastic:$ELASTIC_PASSWORD http://$ELASTIC_HOST:9200/_security/user/remote_monitoring_user/_password -d "{\"password\": \"$BEATS_PASSWORD\"}"
-    curl --silent -X PUT -u elastic:$ELASTIC_PASSWORD http://$ELASTIC_HOST:9200/_security/user/beats_system/_enable
-    curl --silent -X PUT -u elastic:$ELASTIC_PASSWORD http://$ELASTIC_HOST:9200/_security/user/remote_monitoring_user/_enable
 
+if [ $FILEBEAT_PASSWORD ]; then
+    echo "Setting up filebeat password..."
+    curl --silent -X POST -H "Content-Type: application/json" -u elastic:$ELASTIC_PASSWORD http://$ELASTIC_HOST:9200/_security/role/filebeat -d "{\"cluster\":[\"manage_ilm\", \"manage_pipeline\"],\"indices\":[{\"names\":[\"filebeat-*\"],\"privileges\":[\"all\"],\"allow_restricted_indices\":false}],\"applications\":[],\"run_as\":[],\"metadata\":{}}"
+    curl --silent -X POST -H "Content-Type: application/json" -u elastic:$ELASTIC_PASSWORD http://$ELASTIC_HOST:9200/_security/user/filebeat -d "{\"password\": \"$FILEBEAT_PASSWORD\",\"roles\":[\"beats_system\",\"kibana_system\",\"filebeat\"],\"full_name\":null,\"email\":null,\"metadata\":{}}"
 fi
-if [ $LOGSTASH_PASSWORD ]; then
-    echo "Setting up logstash password..."
-    curl --silent -X POST -H "Content-Type: application/json" -u elastic:$ELASTIC_PASSWORD http://$ELASTIC_HOST:9200/_security/user/logstash_system/_password -d "{\"password\": \"$LOGSTASH_PASSWORD\"}"
-    curl --silent -X PUT -u elastic:$ELASTIC_PASSWORD http://$ELASTIC_HOST:9200/_security/user/logstash_system/_enable
 
+if [ $METRICBEAT_PASSWORD ]; then
+    echo "Setting up metricbeat password... (*WARNING: This has to be a superuser for now... :( )"
+    curl --silent -X POST -H "Content-Type: application/json" -u elastic:$ELASTIC_PASSWORD http://$ELASTIC_HOST:9200/_security/user/metricbeat -d "{\"password\": \"$METRICBEAT_PASSWORD\",\"roles\":[\"superuser\"],\"full_name\":null,\"email\":null,\"metadata\":{}}"
+fi
+
+if [ $AL_PASSWORD ]; then
+    echo "Create assemblyline_system role..."
+    curl --silent -X POST -H "Content-Type: application/json" -u elastic:$ELASTIC_PASSWORD http://$ELASTIC_HOST:9200/_security/role/assemblyline_system -d "{\"cluster\":[],\"indices\":[{\"names\":[\"al_metrics_*\",\"alert\",\"cached_*\",\"emptyresult\",\"error\",\"file*\",\"heuristic\",\"result\",\"service*\",\"signature\",\"submission*\",\"user*\",\"vm\",\"workflow\"],\"privileges\":[\"all\"],\"allow_restricted_indices\":false}],\"applications\":[],\"run_as\":[],\"metadata\":{}}"
+
+    echo "Create assemblyline user..."
+    curl --silent -X POST -H "Content-Type: application/json" -u elastic:$ELASTIC_PASSWORD http://$ELASTIC_HOST:9200/_security/user/assemblyline -d "{\"password\":\"$AL_PASSWORD\",\"roles\":[\"assemblyline_system\"],\"full_name\":null,\"email\":null,\"metadata\":{}}"
+fi
+
+if [ $SU_USERNAME ]; then
+    if [ $SU_PASSWORD ]; then
+        echo "Create super user $SU_USERNAME..."
+        curl --silent -X POST -H "Content-Type: application/json" -u elastic:$ELASTIC_PASSWORD http://$ELASTIC_HOST:9200/_security/user/$SU_USERNAME -d "{\"password\":\"$SU_PASSWORD\",\"roles\":[\"superuser\"],\"full_name\":null,\"email\":null,\"metadata\":{}}"
+    fi
+fi
+
+if [ $K_USERNAME ]; then
+    if [ $K_PASSWORD ]; then
+        echo "Create kibana dashboard user $K_USERNAME..."
+        curl --silent -X POST -H "Content-Type: application/json" -u elastic:$ELASTIC_PASSWORD http://$ELASTIC_HOST:9200/_security/user/$K_USERNAME -d "{\"password\":\"$K_PASSWORD\",\"roles\":[\"kibana_user\",\"apm_user\",\"monitoring_user\"],\"full_name\":null,\"email\":null,\"metadata\":{}}"
+    fi
 fi
 
 echo "Randomizing elastic's user password..."
