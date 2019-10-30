685215b500dbb52dc5b05c4ea6ad8797992ab2d9
sgaron-cse <sgaron.cse@gmail.com>
2019-10-29 23:56:05 -0400

    Stop testing against the rebex server

--- test/test_filestore.py
+++ test/test_filestore.py
@@ -33,24 +33,24 @@ def test_https():
 #     assert fs.get('readme.txt') is not None
 
 
-def test_ftp():
-    """
-    Test FTP FileStore by fetching the readme.txt file from
-    Rebex test server.
-    """
-    fs = FileStore('ftp://demo:password@test.rebex.net')
-    assert fs.exists('readme.txt') != []
-    assert fs.get('readme.txt') is not None
+# def test_ftp():
+#     """
+#     Test FTP FileStore by fetching the readme.txt file from
+#     Rebex test server.
+#     """
+#     fs = FileStore('ftp://demo:password@test.rebex.net')
+#     assert fs.exists('readme.txt') != []
+#     assert fs.get('readme.txt') is not None
 
 
-def test_ftps():
-    """
-    Test FTP over TLS FileStore by fetching the readme.txt file from
-    Rebex test server.
-    """
-    fs = FileStore('ftps://demo:password@test.rebex.net')
-    assert fs.exists('readme.txt') != []
-    assert fs.get('readme.txt') is not None
+# def test_ftps():
+#     """
+#     Test FTP over TLS FileStore by fetching the readme.txt file from
+#     Rebex test server.
+#     """
+#     fs = FileStore('ftps://demo:password@test.rebex.net')
+#     assert fs.exists('readme.txt') != []
+#     assert fs.get('readme.txt') is not None
 
 
 def test_file():
