01edc55bd743b93dccfadf854a7b0b1647ed9b4e
sgaron-cse <sgaron.cse@gmail.com>
2019-11-04 12:12:26 -0500

    Combined all 3 nginx container into one that is more secured

--- docker/build_containers.sh
+++ docker/build_containers.sh
@@ -2,11 +2,9 @@
 
 # Build core containers
 (cd elasticsearch && docker build -t sgaroncse/elasticsearch:7.4.0 .)
-(cd apm-server && docker build -t sgaroncse/apm-server:7.4.0 .)
-(cd nginx-ssl-bitbucket && docker build -t sgaroncse/nginx-ssl-bitbucket:1.15.10-1 .)
-(cd nginx-ssl && docker build -t sgaroncse/nginx-ssl:1.15.10-3 .)
-(cd nginx-ssl-dev && docker build -t sgaroncse/nginx-ssl-dev:1.15.10-2 .)
+(cd nginx-ssl && docker build -t sgaroncse/nginx-ssl:1.17.5 .)
 (cd minio && docker build -t sgaroncse/minio .)
 
 # Build default dev containers
 (cd ../.. && docker build -f alv4/docker/al_dev/Dockerfile -t cccs/assemblyline_dev:latest -t cccs/assemblyline_dev:4.0.10 .)
+(cd ../.. && docker build -f alv4/docker/al_management/Dockerfile -t cccs/assemblyline_management:latest .)
--- docker/push_containers.sh
+++ docker/push_containers.sh
@@ -2,11 +2,9 @@
 
 # Push core containers
 docker push sgaroncse/elasticsearch
-docker push sgaroncse/apm-server
 docker push sgaroncse/minio
-docker push sgaroncse/nginx-ssl-bitbucket
 docker push sgaroncse/nginx-ssl
-docker push sgaroncse/nginx-ssl-dev
 
 # Push dev containers
 docker push cccs/assemblyline_dev
+docker push cccs/assemblyline_management
