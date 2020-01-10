44c5dfb315bcf251056836635c322433275e3584
sgaron-cse <sgaron.cse@gmail.com>
2020-01-10 12:38:07 -0500

    API Keys should not be stored or indexed

--- assemblyline/odm/models/user.py
+++ assemblyline/odm/models/user.py
@@ -14,24 +14,25 @@ class ApiKey(odm.Model):
 
 @odm.model(index=True, store=True)
 class User(odm.Model):
-    agrees_with_tos = odm.Optional(odm.Date(index=False, store=False))  # Date the user agree with terms of service
-    api_quota = odm.Integer(default=10, store=False)                    # Max number of concurrent API requests
-    apikeys = odm.Mapping(odm.Compound(ApiKey), default={})             # List of apikeys
+    agrees_with_tos = odm.Optional(odm.Date(index=False, store=False))    # Date the user agree with terms of service
+    api_quota = odm.Integer(default=10, store=False)                      # Max number of concurrent API requests
+    apikeys = odm.Mapping(odm.Compound(ApiKey), default={},
+                          index=False, store=False)                       # Mapping of api keys
     can_impersonate = odm.Boolean(default=False, index=False,
-                                  store=False)                          # Allowed to query on behalf of others
+                                  store=False)                            # Allowed to query on behalf of others
     classification = odm.Classification(
         is_user_classification=True, copyto="__text__",
-        default=Classification.UNRESTRICTED)                            # Max classification for the user
-    dn = odm.Optional(odm.Keyword(store=False, copyto="__text__"))      # User certificate DN
-    email = odm.Optional(odm.Keyword(copyto="__text__"))                # User's email address
+        default=Classification.UNRESTRICTED)                              # Max classification for the user
+    dn = odm.Optional(odm.Keyword(store=False, copyto="__text__"))        # User certificate DN
+    email = odm.Optional(odm.Keyword(copyto="__text__"))                  # User's email address
     groups = odm.List(odm.Keyword(), copyto="__text__",
-                      default=["USERS"])                                # List of groups the user submits to
-    is_active = odm.Boolean(default=True)                               # is the user active
-    name = odm.Keyword(copyto="__text__")                               # Full name of the user
-    otp_sk = odm.Optional(odm.Keyword(index=False, store=False))        # Secret key to generate one time passwords
-    password = odm.Keyword(index=False, store=False)                    # BCrypt hash of the user's password
-    submission_quota = odm.Integer(default=5, store=False)              # Maximum number of concurrent submissions
-    type = odm.List(odm.Enum(values=USER_TYPES), default=['user'])      # Type of user
+                      default=["USERS"])                                  # List of groups the user submits to
+    is_active = odm.Boolean(default=True)                                 # is the user active
+    name = odm.Keyword(copyto="__text__")                                 # Full name of the user
+    otp_sk = odm.Optional(odm.Keyword(index=False, store=False))          # Secret key to generate one time passwords
+    password = odm.Keyword(index=False, store=False)                      # BCrypt hash of the user's password
+    submission_quota = odm.Integer(default=5, store=False)                # Maximum number of concurrent submissions
+    type = odm.List(odm.Enum(values=USER_TYPES), default=['user'])        # Type of user
     u2f_devices = odm.Mapping(odm.Keyword(), index=False,
-                              store=False, default={})                  # Map of u2f security tokens
-    uname = odm.Keyword(copyto="__text__")                              # Username
+                              store=False, default={})                    # Map of u2f security tokens
+    uname = odm.Keyword(copyto="__text__")                                # Username
