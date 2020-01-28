f6a128d00034ab7435f63ba3798f9aabd28b0a6e
sgaron-cse <sgaron.cse@gmail.com>
2020-01-28 16:00:50 -0500

    Conforms to Elastic Common Schema (ECS) format for logging

--- assemblyline/common/logformat.py
+++ assemblyline/common/logformat.py
@@ -17,5 +17,13 @@ except Exception:  # pylint:disable=W0702
 
 AL_SYSLOG_FORMAT = f'{ip} AL %(levelname)8s %(process)5d %(name)20s | %(message)s'
 AL_LOG_FORMAT = f'%(asctime)-16s %(levelname)8s {hostname} %(process)d %(name)30s | %(message)s'
-AL_JSON_FORMAT = f'{{"timestamp": "%(asctime)s", "type": "assemblyline_log", "ip": "{ip}", "hostname": "{hostname}", ' \
-    f'"level": "%(levelname)s", "pid": "%(process)d", "name": "%(name)s", "message": %(message)s}}'
+AL_JSON_FORMAT = f'{{' \
+    f'"@timestamp": "%(asctime)s", ' \
+    f'"event.module": "assemblyline", ' \
+    f'"event.dataset": "%(name)s", ' \
+    f'"host.ip": "{ip}", ' \
+    f'"host.hostname": "{hostname}", ' \
+    f'"log.level": "%(levelname)s", ' \
+    f'"log.logger": "%(name)s", ' \
+    f'"process.pid": "%(process)d", ' \
+    f'"message": %(message)s}}'
