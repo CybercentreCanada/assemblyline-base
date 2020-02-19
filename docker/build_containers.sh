843898a476d4fe603f5890a20126fd34b2bb0a1a
sgaron-cse <sgaron.cse@gmail.com>
2020-02-18 22:31:18 -0500

    Update dev container so the ui can run the ldap auth module

--- docker/build_containers.sh
+++ docker/build_containers.sh
@@ -6,5 +6,5 @@
 (cd minio && docker build -t sgaroncse/minio .)
 
 # Build default dev containers
-(cd ../.. && docker build -f alv4/docker/al_dev/Dockerfile -t cccs/assemblyline_dev:latest -t cccs/assemblyline_dev:4.0.11 .)
+(cd ../.. && docker build -f alv4/docker/al_dev/Dockerfile -t cccs/assemblyline_dev:latest -t cccs/assemblyline_dev:4.0.12 .)
 (cd ../.. && docker build -f alv4/docker/al_management/Dockerfile -t cccs/assemblyline_management:latest .)
