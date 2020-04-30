a6b6e5e5cfc0f6232dbb2adacba412cd8cba917f
Steve Garon <steve.garon@cyber.gc.ca>
2020-04-30 19:56:09 -0400

    Added attack pattern and category + heuristics to alert ODM

--- assemblyline/odm/models/alert.py
+++ assemblyline/odm/models/alert.py
@@ -5,7 +5,7 @@ EXTENDED_SCAN_VALUES = {"submitted", "skipped", "incomplete", "completed"}
 
 
 @odm.model(index=True, store=False)
-class ALResults(odm.Model):                 # Assemblyline result block
+class ALResults(odm.Model):                                      # Assemblyline result block
     attrib = odm.List(odm.Keyword(), default=[], copyto="__text__")          # List of attribution
     av = odm.List(odm.Keyword(), default=[], store=True, copyto="__text__")  # List of AV hits
     behavior = odm.List(odm.Keyword(), default=[], copyto="__text__")        # List of behaviors for the alert
@@ -21,7 +21,7 @@ class ALResults(odm.Model):                 # Assemblyline result block
 
 
 @odm.model(index=True, store=True)
-class File(odm.Model):    # File block
+class File(odm.Model):                              # File block
     md5 = odm.MD5(copyto="__text__")                    # MD5 of the top level file
     name = odm.Keyword(store=False, copyto="__text__")  # Name of the file
     sha1 = odm.SHA1(copyto="__text__")                  # SHA1 hash of the file
@@ -31,9 +31,20 @@ class File(odm.Model):    # File block
 
 
 @odm.model(index=True, store=False)
-class Verdict(odm.Model):
+class Verdict(odm.Model):                           # Verdict Block
     malicious = odm.List(odm.Keyword(), default=[])      # List of user that thinks this submission is malicious
     non_malicious = odm.List(odm.Keyword(), default=[])  # List of user that thinks this submission is non-malicious
+
+
+@odm.model(index=True, store=False)
+class Heuristic(odm.Model):                               # Heuristic block
+    name = odm.List(odm.Keyword(), default=[])                  # List of related Heuristic names
+
+
+@odm.model(index=True, store=False)
+class Attack(odm.Model):                                  # Att&ck block
+    pattern = odm.List(odm.Keyword(), default=[])               # List of related Att&ck patterns
+    category = odm.List(odm.Keyword(), default=[])              # List of related Att&ck categories
 
 
 @odm.model(index=True, store=True)
@@ -41,10 +52,12 @@ class Alert(odm.Model):
     alert_id = odm.Keyword(copyto="__text__")                           # ID of the alert
     al = odm.Compound(ALResults)                                        # Assemblyline result block
     archive_ts = odm.Date(store=False)                                  # Archiving timestamp
+    attack = odm.Compound(Attack)                                       # Attack result block
     classification = odm.Classification()                               # Classification of the alert
     expiry_ts = odm.Optional(odm.Date(store=False))                     # Expiry timestamp
     extended_scan = odm.Enum(values=EXTENDED_SCAN_VALUES, store=False)  # Status of the extended scan
     file = odm.Compound(File)                                           # File block
+    heuristic = odm.Compound(Heuristic)                                 # Heuristic result block
     label = odm.List(odm.Keyword(), copyto="__text__", default=[])      # List of labels applied to the alert
     metadata = odm.Mapping(odm.Keyword(), store=False)                  # Metadata submitted with the file
     owner = odm.Optional(odm.Keyword())                                 # Owner of the alert
