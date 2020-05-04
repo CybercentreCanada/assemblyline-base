2ebcee1486f26a030d3f2e3b40c514041c2e9766
Steve Garon <steve.garon@cyber.gc.ca>
2020-05-03 23:52:13 -0400

    Added classification to submission_summary

--- assemblyline/odm/models/submission_summary.py
+++ assemblyline/odm/models/submission_summary.py
@@ -1,9 +1,12 @@
 from assemblyline import odm
+from assemblyline.common import forge
+Classification = forge.get_classification()
 
 
 @odm.model(index=False, store=False)
 class SubmissionSummary(odm.Model):
-    expiry_ts = odm.Date(index=True)  # Expiry date
-    tags = odm.Text()                 # Tags cache
-    attack_matrix = odm.Text()        # Att&ck Matrix cache
-    heuristics = odm.Text()           # Heuristics cache
+    classification = odm.Classification(default=Classification.UNRESTRICTED)  # Classification of the cache
+    expiry_ts = odm.Date(index=True)                                          # Expiry date
+    tags = odm.Text()                                                         # Tags cache
+    attack_matrix = odm.Text()                                                # Att&ck Matrix cache
+    heuristics = odm.Text()                                                   # Heuristics cache
