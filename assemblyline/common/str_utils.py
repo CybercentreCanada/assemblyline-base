7de1bb26ec3adf2f8ad002819dd2ceace39b7160
cccs-rs <62077998+cccs-rs@users.noreply.github.com>
2020-10-16 10:32:20 -0400

    Revert "AL-128: Replace StringTable where feasible"

--- assemblyline/common/str_utils.py
+++ assemblyline/common/str_utils.py
@@ -168,7 +168,21 @@ class NamedConstants(object):
         # We implement our own getattr mainly to provide the better exception.
         return self._value_map[s]
 
-class EnumMixin(object):
+
+class StringTable(object):
+
+    def __init__(self, name, string_value_list):
+        self._name = name
+        self._value_map = dict(string_value_list)
+        self._reverse_map = dict([(s[1], s[0]) for s in string_value_list])
+
+        # we also import the list as attributes so things like
+        # tab completion and introspection still work.
+        for s in self._value_map.keys():
+            setattr(self, s, s)
+
+    def name_for_value(self, v):
+        return self._reverse_map[v]
 
     def contains_string(self, s):
         return s in self._reverse_map
