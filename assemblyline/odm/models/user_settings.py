1c73d92b11c450fc2438f345169b78633507050f
Steve Garon <steve.garon@cyber.gc.ca>
2020-12-16 13:03:27 -0500

    Allow the current UI to recognize and interact with the new UI

--- assemblyline/odm/models/user_settings.py
+++ assemblyline/odm/models/user_settings.py
@@ -25,3 +25,5 @@ class UserSettings(odm.Model):                                      # User's def
     services = odm.Compound(ServiceSelection, default={})             # Default service selection
     submission_view = odm.Enum(values=VIEWS, default="report")        # Default view for completed submissions
     ttl = odm.Integer(default=0)                                      # Default submission Time to Live (days)
+    ui4 = odm.Boolean(default=False)                                  # Reroute to UI 4
+    ui4_ask = odm.Boolean(default=True)                               # Ask user if he wants to try UI 4
