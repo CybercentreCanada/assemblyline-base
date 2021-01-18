de2427af2f48620ca7364fa10d981e63cbb252e9
cccs-rs <62077998+cccs-rs@users.noreply.github.com>
2021-01-18 10:22:45 -0500

    Change attack_id from Enum to Keyword (remove validation against attack id list)

--- assemblyline/odm/models/heuristic.py
+++ assemblyline/odm/models/heuristic.py
@@ -1,15 +1,12 @@
 from assemblyline import odm
 from assemblyline.common import forge
-from assemblyline.common.attack_map import attack_map
 
 Classification = forge.get_classification()
-ATTACK_ID_LIST = list(attack_map.keys())
 
 
 @odm.model(index=True, store=True)
 class Heuristic(odm.Model):
-    attack_id = odm.List(odm.Enum(values=ATTACK_ID_LIST, copyto="__text__"),
-                         default=[])                                          # List of all associated Att&ck IDs
+    attack_id = odm.List(odm.Keyword(copyto="__text__"), default=[])          # List of all associated Att&ck IDs
     classification = odm.Classification(default=Classification.UNRESTRICTED)  # Classification of the heuristic
     description = odm.Text(copyto="__text__")                                 # Description of the heuristic
     filetype = odm.Keyword(copyto="__text__")                                 # Type of file targeted
