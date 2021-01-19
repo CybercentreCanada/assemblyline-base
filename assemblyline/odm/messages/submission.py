34e510fe7396aa617c67c33b697b03fe963434fc
Adam Douglass <adam.douglass@cyber.gc.ca>
2021-01-19 16:36:53 -0500

    Adjust the submission message types, add helper function

--- assemblyline/odm/messages/submission.py
+++ assemblyline/odm/messages/submission.py
@@ -1,8 +1,8 @@
 from typing import List, Dict
 from assemblyline import odm
-from assemblyline.odm.models.submission import SubmissionParams, File
+from assemblyline.odm.models.submission import SubmissionParams, File, Submission as DatabaseSubmission
 
-MSG_TYPES = {"SubmissionIngested", "SubmissionReceived", "SubmissionQueued", "SubmissionStarted"}
+MSG_TYPES = {"SubmissionIngested", "SubmissionReceived", "SubmissionStarted", "SubmissionCompleted"}
 LOADER_CLASS = "assemblyline.odm.messages.submission.SubmissionMessage"
 
 
@@ -22,9 +22,18 @@ class Submission(odm.Model):
     params: SubmissionParams = odm.Compound(SubmissionParams)            # Parameters of the submission
 
 
+def from_datastore_submission(submission: DatabaseSubmission):
+    return Submission({
+        'sid': submission.sid,
+        'files': submission.files,
+        'metadata': submission.metadata,
+        'params': submission.params
+    })
+
+
 @odm.model()
 class SubmissionMessage(odm.Model):
     msg = odm.Compound(Submission)                                       # Body of the message
     msg_loader = odm.Enum(values={LOADER_CLASS}, default=LOADER_CLASS)   # Class to use to load the message as an object
-    msg_type = odm.Enum(values=MSG_TYPES, default="SubmissionReceived")  # Type of message
+    msg_type = odm.Enum(values=MSG_TYPES)                                # Type of message
     sender = odm.Keyword()                                               # Sender of the message
