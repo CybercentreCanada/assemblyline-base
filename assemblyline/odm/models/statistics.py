f82588b6433cf1598f2cdb54934f26eef34aa1b9
Steve Garon <steve.garon@cyber.gc.ca>
2021-02-08 10:43:12 -0500

    Added stats odm to heuristic and signature odm

---
+++ assemblyline/odm/models/statistics.py
@@ -0,0 +1,12 @@
+from assemblyline import odm
+
+
+@odm.model(index=True, store=False)
+class Statistics(odm.Model):
+    count = odm.Integer(default=0, store=True)
+    min = odm.Integer(default=0)
+    max = odm.Integer(default=0)
+    avg = odm.Integer(default=0)
+    sum = odm.Integer(default=0)
+    first_hit = odm.Optional(odm.Date())
+    last_hit = odm.Optional(odm.Date(store=True))
