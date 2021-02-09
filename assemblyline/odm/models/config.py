0f0bc82a1ace63cee2c471c243dcb93d89a82aaf
cccs-rs <62077998+cccs-rs@users.noreply.github.com>
2021-02-09 13:15:31 -0500

    add additional_labels to scaler config

--- assemblyline/odm/models/config.py
+++ assemblyline/odm/models/config.py
@@ -508,6 +508,8 @@ class Scaler(odm.Model):
     # only available for docker hosts, not kubernetes
     cpu_overallocation: float = odm.Float(default=1)
     memory_overallocation: float = odm.Float(default=1)
+    # Additional labels to be applied to deployments in kubernetes
+    additional_labels: List[str] = odm.Optional(odm.Mapping(odm.Text()))
 
 
 DEFAULT_SCALER = {
