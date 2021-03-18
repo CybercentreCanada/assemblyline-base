ed2c5acc74a95d53d429a1e3f98fa94f7b452820
Steve Garon <steve.garon@cyber.gc.ca>
2021-03-18 14:31:09 +0000

    Add malicious flag to default user settings

--- assemblyline/odm/models/user_settings.py
+++ assemblyline/odm/models/user_settings.py
@@ -19,6 +19,7 @@ class UserSettings(odm.Model):                                      # User's def
     ignore_cache = odm.Boolean(default=False)                         # Ignore service caching
     ignore_dynamic_recursion_prevention = odm.Boolean(default=False)  # Ignore dynamic recursion prevention
     ignore_filtering = odm.Boolean(default=False)                     # Ignore filtering services
+    malicious = odm.Boolean(default=False)                            # Is the file submitted known to be malicious
     priority = odm.Integer(default=1000)                              # Default priority for the submissions
     profile = odm.Boolean(default=False)                              # Should the submission do extra profiling
     service_spec = odm.Mapping(odm.Keyword(), default={})             # Default service specific settings
