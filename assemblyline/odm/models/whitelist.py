1a6ba78a74263fe848add0bbcaa19d5643b87c51
Steve Garon <steve.garon@cyber.gc.ca>
2021-06-04 12:23:39 +0000

    Change the structure of the whitelist ODM

--- assemblyline/odm/models/whitelist.py
+++ assemblyline/odm/models/whitelist.py
@@ -3,15 +3,25 @@ from assemblyline import odm
 TYPES = ["user", "external"]
 
 
-@odm.model(index=True, store=True)
+@odm.model(index=True, store=False)
+class FileInfo(odm.Model):
+    md5 = odm.Optional(odm.MD5())                   # MD5 hash of the file
+    sha1 = odm.Optional(odm.SHA1())                 # SHA1 Hash of the file
+    sha256 = odm.SHA256(store=True)                 # SHA256 Hash of the file
+    size = odm.Optional(odm.Integer())              # Size of the file
+    type = odm.Optional(odm.Keyword())              # Type of file
+
+
+@odm.model(index=True, store=False)
 class Source(odm.Model):
-    name = odm.Keyword()                            # Name of the source
+    name = odm.Keyword(store=True)                  # Name of the source
     type = odm.Enum(values=TYPES)                   # Type of source
     reason = odm.List(odm.Keyword(), default=[])    # Reason why it is whitelisted
 
 
 @odm.model(index=True, store=True)
 class Whitelist(odm.Model):
-    hash = odm.SHA256()
-    date = odm.Date(default="NOW")                         # Whitelisting date
-    sources = odm.List(odm.Compound(Source), default=[])   # List of reasons why it is whitelisted
+    added = odm.Date(default="NOW")                       # Date when the hash was added
+    fileinfo = odm.Compound(FileInfo)                     # Informations about the file
+    updated = odm.Date(default="NOW")                     # Last date when sources were added to the hash
+    sources = odm.List(odm.Compound(Source), default=[])  # List of reasons why it is whitelisted
