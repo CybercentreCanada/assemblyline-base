19e608392338b8b31e0939428c961939dfe7a172
Adam <adam.douglass@cyber.gc.ca>
2021-07-30 10:08:01 -0400

    Add a few redis tests

--- test/test_metrics.py
+++ test/test_metrics.py
@@ -5,6 +5,7 @@ import pytest
 from assemblyline.common.metrics import MetricsFactory, PerformanceTimer
 from assemblyline import odm
 from assemblyline.common import forge
+from assemblyline.remote.datatypes.exporting_counter import export_metrics_once
 
 
 @odm.model()
@@ -45,3 +46,31 @@ def test_metrics_counter(redis_connection):
     assert read['performance_counter.c'] == 2
 
     source.stop()
+
+
+def test_metrics_export(redis_connection):
+    channel = forge.get_metrics_sink(redis_connection)
+    channel.listen(blocking=False)
+
+    export_metrics_once('test', Metrics, {'counter': 55, 'performance_counter.t': 12, 'performance_counter.c': 2})
+
+    start = time.time()
+    read = {}
+    for metric_message in channel.listen(blocking=False):
+        if 'counter' in read and 'performance_counter.t' in read:
+            break
+
+        if time.time() - start > 30:
+            pytest.fail()
+
+        if metric_message is None:
+            time.sleep(0.1)
+            continue
+
+        for key, value in metric_message.items():
+            if isinstance(value, (int, float)):
+                read[key] = read.get(key, 0) + value
+
+    assert read['counter'] == 55
+    assert read['performance_counter.t'] == 12
+    assert read['performance_counter.c'] == 2
