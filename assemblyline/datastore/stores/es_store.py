40b4c7ee4f0e3c5ed1fa13db71533c726466c417
Steve Garon <steve.garon@cyber.gc.ca>
2021-11-03 12:42:14 +0000

    Remove force_achive_access for save function

--- assemblyline/datastore/stores/es_store.py
+++ assemblyline/datastore/stores/es_store.py
@@ -774,7 +774,7 @@ class ESCollection(Collection):
             return None, CREATE_TOKEN
         return None
 
-    def _save(self, key, data, force_archive_access=False, version=None):
+    def _save(self, key, data, version=None):
         if self.model_class:
             saved_data = data.as_primitives(hidden_fields=True)
         else:
