2485b7355efff80f9563f98abf88cf1418c38c4e
cccs-rs <ryan.samaroo@cyber.gc.ca>
2022-04-07 19:30:53 +0000

    Add descriptions to the rest in 'models'

--- assemblyline/odm/models/user_favorites.py
+++ assemblyline/odm/models/user_favorites.py
@@ -3,20 +3,20 @@ from assemblyline.common import forge
 Classification = forge.get_classification()
 
 
-@odm.model(index=False, store=False)
+@odm.model(index=False, store=False, description="Abstract Model of Favorite")
 class Favorite(odm.Model):
-    created_by = odm.Keyword()                           # Who created the favorite
-    classification = odm.Classification(
-        is_user_classification=True, copyto="__text__",
-        default=Classification.UNRESTRICTED)             # Classification of the favorite
-    name = odm.Keyword()                                 # Name of the favorite
-    query = odm.Keyword()                                # Query for the favorite
+    created_by = odm.Keyword(description="Who created the favorite")
+    classification = odm.Classification(is_user_classification=True, copyto="__text__",
+                                        default=Classification.UNRESTRICTED,
+                                        description="Classification of the favorite")
+    name = odm.Keyword(description="Name of the favorite")
+    query = odm.Keyword(description="Query for the favorite")
 
 
-@odm.model(index=False, store=False)
+@odm.model(index=False, store=False, description="Model of User Favorites")
 class UserFavorites(odm.Model):
-    alert = odm.List(odm.Compound(Favorite), default=[])       # Alert page favorites
-    error = odm.List(odm.Compound(Favorite), default=[])       # Error page favorites
-    search = odm.List(odm.Compound(Favorite), default=[])      # Search page favorites
-    signature = odm.List(odm.Compound(Favorite), default=[])   # Signature page favorites
-    submission = odm.List(odm.Compound(Favorite), default=[])  # Submission page favorites
+    alert = odm.List(odm.Compound(Favorite), default=[], description="Alert page favorites")
+    error = odm.List(odm.Compound(Favorite), default=[], description="Error page favorites")
+    search = odm.List(odm.Compound(Favorite), default=[], description="Search page favorites")
+    signature = odm.List(odm.Compound(Favorite), default=[], description="Signature page favorites")
+    submission = odm.List(odm.Compound(Favorite), default=[], description="Submission page favorites")
--- assemblyline/odm/models/user_settings.py
+++ assemblyline/odm/models/user_settings.py
@@ -8,21 +8,22 @@ ENCODINGS = {"cart", "raw"}
 VIEWS = {"report", "details"}
 
 
-@odm.model(index=False, store=False)
-class UserSettings(odm.Model):                                      # User's default settings
-    classification = odm.Classification(
-        default=Classification.UNRESTRICTED)                          # Default submission classification
-    deep_scan = odm.Boolean(default=False)                            # Should a deep scan be performed
-    description = odm.Keyword(default="")                             # Default description
-    download_encoding = odm.Enum(values=ENCODINGS, default="cart")    # Default download encoding when downloading files
-    expand_min_score = odm.Integer(default=500)                       # Auto-expand section when score bigger then this
-    ignore_cache = odm.Boolean(default=False)                         # Ignore service caching
-    ignore_dynamic_recursion_prevention = odm.Boolean(default=False)  # Ignore dynamic recursion prevention
-    ignore_filtering = odm.Boolean(default=False)                     # Ignore filtering services
-    malicious = odm.Boolean(default=False)                            # Is the file submitted known to be malicious
-    priority = odm.Integer(default=1000)                              # Default priority for the submissions
-    profile = odm.Boolean(default=False)                              # Should the submission do extra profiling
-    service_spec = odm.Mapping(odm.Mapping(odm.Any()), default={})    # Default service specific settings
-    services = odm.Compound(ServiceSelection, default={})             # Default service selection
-    submission_view = odm.Enum(values=VIEWS, default="report")        # Default view for completed submissions
-    ttl = odm.Integer(default=30)                                     # Default submission Time to Live (days)
+@odm.model(index=False, store=False, description="Model of User Settings")
+class UserSettings(odm.Model):
+    classification = odm.Classification(default=Classification.UNRESTRICTED,
+                                        description="Default submission classification")
+    deep_scan = odm.Boolean(default=False, description="Should a deep scan be performed?")
+    description = odm.Keyword(default="", description="Default description")
+    download_encoding = odm.Enum(values=ENCODINGS, default="cart",
+                                 description="Default download encoding when downloading files")
+    expand_min_score = odm.Integer(default=500, description="Auto-expand section when score bigger then this")
+    ignore_cache = odm.Boolean(default=False, description="Ignore service caching?")
+    ignore_dynamic_recursion_prevention = odm.Boolean(default=False, description="Ignore dynamic recursion prevention?")
+    ignore_filtering = odm.Boolean(default=False, description="Ignore filtering services?")
+    malicious = odm.Boolean(default=False, description="Is the file submitted already known to be malicious?")
+    priority = odm.Integer(default=1000, description="Default priority for the submissions")
+    profile = odm.Boolean(default=False, description="Should the submission do extra profiling?")
+    service_spec = odm.Mapping(odm.Mapping(odm.Any()), default={}, description="Default service specific settings")
+    services = odm.Compound(ServiceSelection, default={}, description="Default service selection")
+    submission_view = odm.Enum(values=VIEWS, default="report", description="Default view for completed submissions")
+    ttl = odm.Integer(default=30, description="Default submission TTL, in days")
