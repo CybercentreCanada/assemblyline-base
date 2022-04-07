ae9363500ea1b81dd06107e0a3d762ee0a04b5e3
cccs-rs <ryan.samaroo@cyber.gc.ca>
2022-04-07 19:34:46 +0000

    remove random import

--- assemblyline/odm/models/config.py
+++ assemblyline/odm/models/config.py
@@ -2,7 +2,6 @@ from typing import Dict, List
 
 from assemblyline import odm
 from assemblyline.odm.models.service import EnvironmentVariable
-from pandas import describe_option
 
 OAUTH_AUTO_PROPERTY_TYPE = ['access', 'classification', 'role']
 
