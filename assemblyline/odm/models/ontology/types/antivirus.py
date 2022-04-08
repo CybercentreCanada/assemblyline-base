e618fb1aa52dc1cdf5bde53e48817b0f6736ec0a
cccs-rs <ryan.samaroo@cyber.gc.ca>
2022-04-08 14:56:44 +0000

    Add descriptions to ontology models

--- assemblyline/odm/models/ontology/types/antivirus.py
+++ assemblyline/odm/models/ontology/types/antivirus.py
@@ -1,21 +1,30 @@
 from assemblyline import odm
 
 
+@odm.model(description="Antivirus Ontology Model")
 class Antivirus(odm.Model):
+    @odm.model(description="Antivirus Detection Model")
     class Detection(odm.Model):
+        @odm.model(description="Antivirus Engine Model")
         class Engine(odm.Model):
-            definition_version = odm.Optional(odm.Keyword())                    # Version of definition set
-            name = odm.Keyword()                                                # Name of AV engine
-            version = odm.Optional(odm.Keyword())                               # Version of AV engine
+            definition_version = odm.Optional(odm.Keyword(), description="Version of definition set")
+            name = odm.Keyword(description="Name of antivirus engine")
+            version = odm.Optional(odm.Keyword(), description="Version of antivirus engine")
 
         # What category does the verdict fall under?
-        category = odm.Optional(odm.Enum(['type-unsupported',                   # File sent to AV is unsupported
-                                          'undetected',                         # File not detected by AV
-                                          'failure',                            # AV failed during detection
-                                          'suspicious',                         # AV deems suspicious
-                                          'malicious']))                        # AV deems malicious
-        engine = odm.Compound(Engine)
-        virus_name = odm.Optional(odm.Keyword())                                # The name of the virus
+        category = odm.Optional(odm.Enum(['type-unsupported',
+                                          'undetected',
+                                          'failure',
+                                          'suspicious',
+                                          'malicious']),
+                                description="What category does the verdict fall under?<br><ul>"
+                                "<li>`type-unsupported`: File sent to antivirus is unsupported</li>"
+                                "<li>`undetected`: File not detected by antivirus</li>"
+                                "<li>`failure`: Antivirus failed during detection</li>"
+                                "<li>`suspicious`: Antivirus deems suspicious</li>"
+                                "<li>`malicious`: Antivirus deems malicious</li></ul>")
+        engine = odm.Compound(Engine, description="Antivirus engine information")
+        virus_name = odm.Optional(odm.Keyword(), description="The name of the virus")
 
-    odm_version = odm.Text(default="1.0")                                       # Version of AV ontological result
-    detections = odm.List(odm.Compound(Detection))                              # List of AV detections
+    odm_version = odm.Text(default="1.0", description="Version of antivirus ontological result")
+    detections = odm.List(odm.Compound(Detection), description="List of antivirus detections")
--- assemblyline/odm/models/ontology/types/sandbox.py
+++ assemblyline/odm/models/ontology/types/sandbox.py
@@ -4,76 +4,56 @@ from assemblyline.odm.models.ontology.types.process import Process
 from assemblyline.odm.models.ontology.types.network import NetworkConnection, NetworkDNS, NetworkHTTP
 
 
-# The result ontology for sandbox output
+@odm.model(description="Sandbox Ontology Model")
 class Sandbox(odm.Model):
-
-    # The metadata of the analysis, per analysis
+    @odm.model(description="The metadata of the analysis, per analysis")
     class AnalysisMetadata(odm.Model):
-
-        # The metadata regarding the machine where the analysis took place
+        @odm.model(description="The metadata regarding the machine where the analysis took place")
         class MachineMetadata(odm.Model):
-            # The IP of the machine used for analysis
-            ip = odm.Optional(odm.IP())
-            # The hypervisor of the machine used for analysis
-            hypervisor = odm.Optional(odm.Keyword())
-            # The name of the machine used for analysis
-            hostname = odm.Optional(odm.Keyword())
-            # The platform of the machine used for analysis
-            platform = odm.Optional(odm.Platform())
-            # The version of the operating system of the machine used for analysis
-            version = odm.Optional(odm.Keyword())
-            # The architecture of the machine used for analysis
-            architecture = odm.Optional(odm.Processor())
+            ip = odm.Optional(odm.IP(), description="The IP of the machine used for analysis")
+            hypervisor = odm.Optional(odm.Keyword(), description="The hypervisor of the machine used for analysis")
+            hostname = odm.Optional(odm.Keyword(), description="The name of the machine used for analysis")
+            platform = odm.Optional(odm.Platform(), description="The platform of the machine used for analysis")
+            version = odm.Optional(odm.Keyword(),
+                                   description="The version of the operating system of the machine used for analysis")
+            architecture = odm.Optional(odm.Processor(),
+                                        description="The architecture of the machine used for analysis")
 
-        # The ID used for identifying the analysis task
-        task_id = odm.Optional(odm.Keyword())
-        # The start time of the analysis
-        start_time = odm.Date()
-        # The end time of the analysis
-        end_time = odm.Date()
-        # The routing used in the sandbox setup (Spoofed, Internet, Tor, VPN)
-        routing = odm.Optional(odm.Keyword())
-        # The metadata of the analysis
-        machine_metadata = odm.Optional(odm.Compound(MachineMetadata))
+        task_id = odm.Optional(odm.Keyword(), description="The ID used for identifying the analysis task")
+        start_time = odm.Date(description="The start time of the analysis")
+        end_time = odm.Date(description="The end time of the analysis")
+        routing = odm.Optional(odm.Keyword(),
+                               description="The routing used in the sandbox setup (Spoofed, Internet, Tor, VPN)")
+        machine_metadata = odm.Optional(odm.Compound(MachineMetadata), description="The metadata of the analysis")
 
-    # A signature that was raised during the analysis of the task
+    @odm.model(description="A signature that was raised during the analysis of the task")
     class Signature(odm.Model):
 
-        # The subject of the signature, aka something interesting that the signature was raised on that is worth reporting
+        @odm.model(description="The subject of the signature, aka something interesting that the signature was raised on that is worth reporting")
         class Subject(odm.Model):
-            ip = odm.Optional(odm.IP())
-            domain = odm.Optional(odm.Domain())
-            uri = odm.Optional(odm.URI())
-            process = odm.Optional(odm.Compound(Process))
-            file = odm.Optional(odm.Text())
-            registry = odm.Optional(odm.Text())
+            ip = odm.Optional(odm.IP(), description="Subject's IP")
+            domain = odm.Optional(odm.Domain(), description="Subject's domain")
+            uri = odm.Optional(odm.URI(), description="Subject's URI")
+            process = odm.Optional(odm.Compound(Process), description="Subject's process")
+            file = odm.Optional(odm.Text(), description="Subject's file")
+            registry = odm.Optional(odm.Text(), description="Subject's registry key")
 
-        # The name of the signature
-        name = odm.Keyword()
-        # The process associated with the signature
-        process = odm.Optional(odm.Compound(Process))
-        # A list of subjects. A signature can have more than one subject.
-        subjects = odm.Optional(odm.List(odm.Compound(Subject)))
-        # The description of the signature
-        description = odm.Optional(odm.Keyword())
-        # A list of Att&ck patterns and categories of the signature
-        attack = odm.Optional(odm.List(odm.Compound(Attack)))
+        name = odm.Keyword(description="The name of the signature")
+        process = odm.Optional(odm.Compound(Process), description="The process associated with the signature")
+        subjects = odm.Optional(odm.List(odm.Compound(Subject)),
+                                description="A list of subjects. A signature can have more than one subject.")
+        description = odm.Optional(odm.Keyword(), description="The description of the signature")
+        attack = odm.Optional(odm.List(odm.Compound(Attack)),
+                              description="A list of Att&ck patterns and categories of the signature")
 
-    # Metadata for the analysis
-    analysis_metadata = odm.Compound(AnalysisMetadata)
-    # A list of processes
-    processes = odm.List(odm.Compound(Process), default=[])
-    # The IP traffic observed during analysis
-    network_connections = odm.List(odm.Compound(NetworkConnection), default=[])
-    # Signatures that the file may have
-    signatures = odm.List(odm.Compound(Signature), default=[])
-    # The DNS traffic observed during analysis
-    network_dns = odm.List(odm.Compound(NetworkDNS), default=[])
-    # The HTTP traffic observed during analysis
-    network_http = odm.List(odm.Compound(NetworkHTTP), default=[])
-    # The name of the sandbox
-    sandbox_name = odm.Keyword()
-    # The version of the sandbox
-    sandbox_version = odm.Optional(odm.Keyword())
-    # Version of AV ontological result
-    odm_version = odm.Text(default="1.0")
+    analysis_metadata = odm.Compound(AnalysisMetadata, description="Metadata for the analysis")
+    processes = odm.List(odm.Compound(Process), default=[], description="A list of processes")
+    network_connections = odm.List(odm.Compound(NetworkConnection), default=[],
+                                   description="The IP traffic observed during analysis")
+    signatures = odm.List(odm.Compound(Signature), default=[], description="Signatures that the file may have")
+    network_dns = odm.List(odm.Compound(NetworkDNS), default=[], description="The DNS traffic observed during analysis")
+    network_http = odm.List(odm.Compound(NetworkHTTP), default=[],
+                            description="The HTTP traffic observed during analysis")
+    sandbox_name = odm.Keyword(description="The name of the sandbox")
+    sandbox_version = odm.Optional(odm.Keyword(), description="The version of the sandbox")
+    odm_version = odm.Text(default="1.0", description="Version of sandbox ontological result")
