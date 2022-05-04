c9b577f10d4c7847759610cd3462f93c6bd1ddcb
Steve Garon <steve.garon@cyber.gc.ca>
2022-05-04 16:43:17 +0000

    Log errors

--- assemblyline/common/language.py
+++ assemblyline/common/language.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import re
 import yara
@@ -7,6 +8,8 @@ from typing import Tuple, Union, Dict
 
 from assemblyline.common.forge import get_constants
 from assemblyline.common.str_utils import safe_str
+
+LOGGER = logging.getLogger('assemblyline.identify')
 
 STRONG_INDICATORS = {
     "code/csharp": [
@@ -269,9 +272,13 @@ rules = yara.compile(filepaths=rules_list, externals=default_externals)
 
 def guess_language(path: str, info: Dict, fallback="unknown") -> Tuple[str, Union[str, int]]:
     externals = {k: v or "" for k, v in info.items() if k in default_externals}
-    matches = rules.match(path, externals=externals, fast=True)
-    matches.sort(key=lambda x: x.meta.get('score', 0), reverse=True)
-    for match in matches:
-        return match.meta['type']
+    try:
+        matches = rules.match(path, externals=externals, fast=True)
+        matches.sort(key=lambda x: x.meta.get('score', 0), reverse=True)
+        for match in matches:
+            return match.meta['type']
+    except Exception as e:
+        LOGGER.warning(f"Yara file identifier failed with error: {str(e)}")
+        matches = []
 
     return fallback
