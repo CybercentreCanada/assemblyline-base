e6ec2565cf412c479859faf71dcf0378ee472c40
cccs-kevin <kevin.hardy-cooper@cyber.gc.ca>
2022-05-25 15:37:46 +0000

    Updating model with pluralization, dns records, optional fields

--- assemblyline/odm/models/ontology/results/network.py
+++ assemblyline/odm/models/ontology/results/network.py
@@ -11,7 +11,16 @@ OID_PARTS = ['source_ip', 'source_port',
 class NetworkDNS(odm.Model):
     domain = odm.Domain(description="The domain requested")
     resolved_ips = odm.List(odm.IP(), description="A list of IPs that were resolved")
-    lookup_type = odm.Text(description="The type of DNS request")
+    lookup_type = odm.Enum(values=[
+        # https://en.wikipedia.org/wiki/List_of_DNS_record_types
+        "A", "AAAA", "AFSDB", "ALIAS", "APL", "CAA", "CDNSKEY", "CDS",
+        "CERT", "CNAME", "CSYNC", "DHCID", "DLV", "DNAME", "DNSKEY",
+        "DS", "EUI48", "EUI64", "HINFO", "HIP", "HTTPS", "IPSECKEY",
+        "KEY", "KX", "LOC", "MX", "NAPTR", "NS", "NSEC", "NSEC3",
+        "NSEC3PARAM", "OPENPGPKEY", "PTR", "RRSIG", "RP", "SIG", "SMIMEA",
+        "SOA", "SRV", "SSHFP", "SVCB", "TA", "TKEY", "TLSA", "TSIG", "TXT",
+        "URI", "ZONEMD",
+    ], description="The type of DNS request")
 
 
 @odm.model(description="Details for an HTTP request")
@@ -19,7 +28,7 @@ class NetworkHTTP(odm.Model):
     request_uri = odm.URI(description="The URI requested")
     request_headers = odm.Mapping(odm.Json(), description="Headers included in the request")
     request_body = odm.Optional(odm.Text(), description="The body of the request")
-    request_method = odm.Enum([
+    request_method = odm.Enum(values=[
         # Standard HTTP methods
         "GET", "POST", "PUT", "DELETE", "HEAD", "CONNECT", "OPTIONS", "TRACE", "PATCH",
         # WebDAV HTTP methods
@@ -35,17 +44,21 @@ class NetworkHTTP(odm.Model):
 class Networking(odm.Model):
     oid = odm.Keyword(description="Unique identifier of ontology")
 
-    objectid = odm.Compound(ObjectID, description="The object ID of the process object")
+    objectid = odm.Compound(ObjectID, description="The object ID of the network object")
     process = odm.Optional(odm.Compound(Process), description="The process that spawned the network connection")
     source_ip = odm.Optional(odm.IP(), description="The source IP of the connection")
     source_port = odm.Optional(odm.Integer(), description="The source port of the connection")
     destination_ip = odm.IP(description="The destination IP of the connection")
     destination_port = odm.Integer(description="The destination port of the connection")
-    transport_layer_protocol = odm.Enum(["tcp", "udp"], description="The transport layer protocol of the connection")
-    direction = odm.Enum(["outbound", "inbound", "unknown"], description="The direction of the network connection")
+    transport_layer_protocol = odm.Enum(
+        values=["tcp", "udp"],
+        description="The transport layer protocol of the connection")
+    direction = odm.Enum(
+        values=["outbound", "inbound", "unknown"],
+        description="The direction of the network connection")
     http_details = odm.Optional(odm.Compound(NetworkHTTP), description="HTTP-specific details of request")
     dns_details = odm.Optional(odm.Compound(NetworkDNS), description="DNS-specific details of request")
-    connection_type = odm.Enum(values=['http', 'dns'], description="Type of connection being made")
+    connection_type = odm.Optional(odm.Enum(values=['http', 'dns'], description="Type of connection being made"))
 
     def get_oid(data: dict):
         return f"network_{get_dict_fingerprint_hash({key: data.get(key) for key in OID_PARTS})}"
