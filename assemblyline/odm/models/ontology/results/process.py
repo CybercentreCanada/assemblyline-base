f9c0ffb7b6999af2fcbce74d2a8c52e5a87898a0
cccs-rs <ryan.samaroo@cyber.gc.ca>
2022-05-31 11:38:07 +0000

    make some process fields optional

--- assemblyline/odm/models/ontology/results/process.py
+++ assemblyline/odm/models/ontology/results/process.py
@@ -20,7 +20,7 @@ class Process(odm.Model):
     objectid = odm.Compound(ObjectID, description="The object ID of the process object")
 
     # Parent process details
-    pobjectid = odm.Compound(ObjectID, description="The object ID of the parent process object")
+    pobjectid = odm.Optional(odm.Compound(ObjectID, description="The object ID of the parent process object"))
     pimage = odm.Optional(odm.Text(), description="The image of the parent process that spawned this process")
     pcommand_line = odm.Optional(odm.Text(), description="The command line that the parent process ran")
     ppid = odm.Optional(odm.Integer(), description="The process ID of the parent process")
@@ -29,7 +29,7 @@ class Process(odm.Model):
     image = odm.Text(default="<unknown_image>", description="The image of the process")
     command_line = odm.Optional(odm.Text(), description="The command line that the process ran")
     start_time = odm.Date(description="The time of creation for the process")
-    end_time = odm.Date(description="The time of termination for the process")
+    end_time = odm.Optional(odm.Date(), description="The time of termination for the process")
     integrity_level = odm.Optional(odm.Text(), description="The integrity level of the process")
     image_hash = odm.Optional(odm.Text(), description="The hash of the file run")
     original_file_name = odm.Optional(odm.Text(), description="The original name of the file")
