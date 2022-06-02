1b602b8e6eac2506c379495e334072d7e26790c0
cccs-kevin <59843993+cccs-kevin@users.noreply.github.com>
2022-06-02 11:57:17 -0400

    Make GUID optional, tag mandatory

--- assemblyline/odm/models/ontology/results/process.py
+++ assemblyline/odm/models/ontology/results/process.py
@@ -6,8 +6,8 @@ OID_PARTS = ['pid', 'ppid', 'image', 'command_line']
 
 @odm.model(description="Details about the characteristics used to identify an object")
 class ObjectID(odm.Model):
-    guid = odm.Text(description="The GUID associated with the object")
-    tag = odm.Optional(odm.Text(), description="The normalized tag of the object")
+    guid = odm.Optional(odm.Text(description="The GUID associated with the object"))
+    tag = odm.Text(description="The normalized tag of the object")
     treeid = odm.Optional(odm.Text(), description="The hash of the tree ID")
     processtree = odm.Optional(odm.Keyword(), description="Human-readable tree ID (concatenation of tags)")
     time_observed = odm.Optional(odm.Date(), description="The time at which the object was observed")
