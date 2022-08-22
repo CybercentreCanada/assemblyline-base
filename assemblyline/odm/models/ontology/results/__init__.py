4e75c8fc9d2e8e7df10ee8cf545c274997ef7bf8
cccs-rs <ryan.samaroo@cyber.gc.ca>
2022-08-22 19:26:38 +0000

    Add ontology for MalwareConfigs

--- assemblyline/odm/models/ontology/results/__init__.py
+++ assemblyline/odm/models/ontology/results/__init__.py
@@ -1,4 +1,5 @@
 from assemblyline.odm.models.ontology.results.antivirus import Antivirus
+from assemblyline.odm.models.ontology.results.malware_config import MalwareConfig
 from assemblyline.odm.models.ontology.results.process import Process
 from assemblyline.odm.models.ontology.results.sandbox import Sandbox
 from assemblyline.odm.models.ontology.results.signature import Signature
