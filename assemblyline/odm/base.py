da1bb8bf08ab4b080d2c3b217cda44347466963f
Steve Garon <steve.garon@cyber.gc.ca>
2022-10-13 15:09:30 +0000

    Fix scheme in the full uri

--- assemblyline/odm/base.py
+++ assemblyline/odm/base.py
@@ -72,7 +72,8 @@ SHA1_REGEX = r"^[a-f0-9]{40}$"
 SHA256_REGEX = r"^[a-f0-9]{64}$"
 MAC_REGEX = r"^(?:(?:[0-9a-f]{2}-){5}[0-9a-f]{2}|(?:[0-9a-f]{2}:){5}[0-9a-f]{2})$"
 URI_PATH = r"(?:[/?#]\S*)"
-FULL_URI = f"^((?:(?:[A-Za-z]*:)?//)?(?:\\S+(?::\\S*)?@)?({IP_REGEX}|{DOMAIN_REGEX})(?::\\d{{2,5}})?){URI_PATH}?$"
+FULL_URI = f"^((?:(?:[A-Za-z0-9+-.]{{1,}}:)?//)?(?:\\S+(?::\\S*)?@)?({IP_REGEX}|{DOMAIN_REGEX})(?::\\d{{2,5}})?)" \
+           f"{URI_PATH}?$"
 PLATFORM_REGEX = r"^(Windows|Linux|MacOS|Android|iOS)$"
 PROCESSOR_REGEX = r"^x(64|86)$"
 
