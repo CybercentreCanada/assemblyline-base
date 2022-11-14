df4fae2303614c74e8d17f856929ef217ea9f09e
Adam <adam.douglass@cyber.gc.ca>
2022-11-14 12:41:05 -0500

    add configuration fields for service-accounts

--- assemblyline/odm/models/service.py
+++ assemblyline/odm/models/service.py
@@ -42,6 +42,7 @@ class DockerConfig(odm.Model):
     ports: list[str] = odm.List(odm.Keyword(), default=[], description="What ports of container to expose?")
     ram_mb: int = odm.Integer(default=512, description="Container RAM limit")
     ram_mb_min: int = odm.Integer(default=128, description="Container RAM request")
+    service_account = odm.optional(odm.keyword(description="Service account to use for pods in kubernetes"))
 
 
 @ odm.model(index=False, store=False, description="Container's Persistent Volume Configuration")
