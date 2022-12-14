ae599ff51a82dc9a53c568008a8598bf8df4a063
Adam <adam.douglass@cyber.gc.ca>
2022-12-14 14:40:43 -0500

    Add config field for ingest type

--- assemblyline/odm/models/config.py
+++ assemblyline/odm/models/config.py
@@ -592,6 +592,7 @@ class Vacuum(odm.Model):
     worker_threads: int = odm.Integer()
     worker_rollover: int = odm.Integer()
     minimum_classification: str = odm.Keyword()
+    ingest_type = odm.keyword()
 
 
 DEFAULT_VACUUM = dict(
@@ -608,6 +609,7 @@ DEFAULT_VACUUM = dict(
     worker_threads=50,
     worker_rollover=1000,
     minimum_classification='U',
+    ingest_type='VACUUM',
 )
 
 
