41cbe6b000c31c09bb7c1181695a6783393d2077
cccs-rs <ryan.samaroo@cyber.gc.ca>
2023-01-25 16:50:43 +0000

    Only verify against RootCA if given

--- assemblyline/filestore/transport/s3.py
+++ assemblyline/filestore/transport/s3.py
@@ -57,8 +57,10 @@ class TransportS3(Transport):
         self.endpoint_url = "{scheme}://{host}:{port}".format(scheme=self.scheme, host=self.host, port=self.port)
 
         config = None
-        if self.use_ssl:
-            verify = os.environ.get(f'{host.upper()}_ROOT_CA_PATH', '/etc/assemblyline/ssl/al_root-ca.crt')
+        host_root_ca = os.environ.get(f'{host.upper()}_ROOT_CA_PATH', '/etc/assemblyline/ssl/al_root-ca.crt')
+        if self.use_ssl and os.path.exists(host_root_ca):
+            # Verify against the Root CA associated to the filestore host
+            verify = host_root_ca
 
         session = boto3.session.Session()
         self.client = session.client(
