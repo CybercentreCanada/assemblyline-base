d76c82703bc8e6af97ba1bed87b577af2dba589c
Adam Douglass <Adam.Douglass@cyber.gc.ca>
2023-03-09 16:34:38 +0000

    add default search info for retrohunt queries

--- assemblyline/odm/models/retrohunt.py
+++ assemblyline/odm/models/retrohunt.py
@@ -4,15 +4,15 @@ from assemblyline import odm
 @odm.model(index=True, store=True, description="A search run on stored files.")
 class Retrohunt(odm.Model):
     # Metadata
-    creator = odm.keyword()
-    tags = odm.mapping(odm.sequence(odm.keyword()))
-    description = odm.Text()
+    creator = odm.keyword(copyto="__text__")
+    tags = odm.mapping(odm.sequence(odm.keyword(copyto="__text__")))
+    description = odm.Text(copyto="__text__")
     created = odm.date(default='NOW', description="Start time for the search.")
     classification = odm.Classification(description="Classification of the search")
     # expiry_ts = odm.Optional(odm.Date(store=False), description="Expiry timestamp")
 
     # Search data
-    yara_signature = odm.keyword()
+    yara_signature = odm.keyword(copyto="__text__")
     raw_query = odm.keyword()
     code = odm.keyword()
 
