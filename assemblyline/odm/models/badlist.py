13e9b2e5ddf690cdf8dcfcdb3dc27fc4d2777dab
Steve Garon <steve.garon@cyber.gc.ca>
2023-10-20 14:59:51 +0000

    Added attribution and hashes to badlist

--- assemblyline/odm/models/badlist.py
+++ assemblyline/odm/models/badlist.py
@@ -6,11 +6,24 @@ BADHASH_TYPES = ["file", "tag"]
 SOURCE_TYPES = ["user", "external"]
 
 
+@odm.model(index=True, store=False, description="Attribution Tag Model")
+class Attribution(odm.Model):
+    actor = odm.Optional(odm.List(odm.UpperKeyword(copyto="__text__")), description="Attribution Actor")
+    campaign = odm.Optional(odm.List(odm.UpperKeyword(copyto="__text__")), description="Attribution Campaign")
+    category = odm.Optional(odm.List(odm.UpperKeyword(copyto="__text__")), description="Attribution Category")
+    exploit = odm.Optional(odm.List(odm.UpperKeyword(copyto="__text__")), description="Attribution Exploit")
+    implant = odm.Optional(odm.List(odm.UpperKeyword(copyto="__text__")), description="Attribution Implant")
+    family = odm.Optional(odm.List(odm.UpperKeyword(copyto="__text__")), description="Attribution Family")
+    network = odm.Optional(odm.List(odm.UpperKeyword(copyto="__text__")), description="Attribution Network")
+
+
 @odm.model(index=True, store=True, description="Hashes of a badlisted file")
 class Hashes(odm.Model):
     md5 = odm.Optional(odm.MD5(copyto="__text__"), description="MD5")
     sha1 = odm.Optional(odm.SHA1(copyto="__text__"), description="SHA1")
     sha256 = odm.Optional(odm.SHA256(copyto="__text__"), description="SHA256")
+    ssdeep = odm.Optional(odm.SSDeepHash(copyto="__text__"), description="SSDEEP")
+    tlsh = odm.Optional(odm.Keyword(copyto="__text__", description="TLSH"))
 
 
 @odm.model(index=True, store=False, description="File Details")
@@ -39,6 +52,7 @@ class Tag(odm.Model):
 @odm.model(index=True, store=True, description="Badlist Model")
 class Badlist(odm.Model):
     added = odm.Date(default="NOW", description="Date when the badlisted hash was added")
+    attribution = odm.Optional(odm.Compound(Attribution), description="Attribution related to the bad hash")
     classification = odm.Classification(description="Computed max classification for the bad hash")
     enabled = odm.Boolean(default=True, description="Is bad hash enabled or not?")
     hashes = odm.Compound(Hashes, default={}, description="List of hashes related to the bad hash")
