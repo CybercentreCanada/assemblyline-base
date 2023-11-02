7e4a7fa58237ab9ac91e0ab26e583af362e17913
Steve Garon <steve.garon@cyber.gc.ca>
2023-11-02 20:03:49 +0000

    Added file_purge role

--- assemblyline/odm/models/user.py
+++ assemblyline/odm/models/user.py
@@ -46,6 +46,7 @@ ROLES = StringTable('ROLES', [
     ("retrohunt_view", 28),
     ("retrohunt_run", 29),
     ("external_query", 30),
+    ("file_purge", 31),
 ])
 
 
@@ -92,6 +93,7 @@ USER_ROLES_BASIC = {
 
 USER_ROLES = USER_ROLES_BASIC.union({
     ROLES.administration,      # Perform administrative tasks
+    ROLES.file_purge,          # Purge files from the filestore
     ROLES.replay_system,       # Manage status of file/submission/alerts during the replay process
     ROLES.signature_import,    # Import signatures in the system
     ROLES.signature_manage,    # Manage signatures sources in the system
@@ -163,6 +165,7 @@ ACL_MAP = {
     "E": [
         ROLES.administration,
         ROLES.apikey_access,
+        ROLES.file_purge,
         ROLES.obo_access,
         ROLES.replay_system,
         ROLES.self_manage,
