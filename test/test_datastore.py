e5bb9331002d8ad2f321b9a2216caa626e5b4722
cccs-nr <nicolas.rheaume@cyber.gc.ca>
2023-11-15 20:05:44 +0000

    changes to test datastore

--- test/test_datastore.py
+++ test/test_datastore.py
@@ -58,11 +58,6 @@ with warnings.catch_warnings():
                     "text": "UPDATE_SET"
                 },
                 {
-                    "cid": "5D13pJW8kU2JUSVjlgkRk5",
-                    "uname": "admin",
-                    "text": "UPDATE_MODIFY"
-                },
-                {
                     "cid": "2qTELdsSNSSBHfUf3phtOM",
                     "uname": "admin",
                     "text": "UPDATE_APPEND"
@@ -300,10 +295,6 @@ def _test_update_list_of_objects(c: ESCollection):
             },
             {
                 "uname": "admin",
-                "text": "UPDATE_MODIFY"
-            },
-            {
-                "uname": "admin",
                 "text": "UPDATE_APPEND"
             }, {
                 "uname": "admin",
@@ -355,17 +346,7 @@ def _test_update_list_of_objects(c: ESCollection):
             "uname": "admin",
             "text": "UPDATE_REMOVE"
         }),
-        (c.UPDATE_DELETE, "list_of_objects", 2),
-        (c.UPDATE_MODIFY, "list_of_objects", {
-            "prev":  {
-                "uname": "admin",
-                "text": "UPDATE_SET"
-            },
-            "next": {
-                "uname": "admin",
-                "text": "UPDATE_MODIFY"
-            }
-        })
+        (c.UPDATE_DELETE, "list_of_objects", 2)
     ]
     assert c.update('to_update_list_of_objects', operations)
     assert c.get('to_update_list_of_objects') == expected
