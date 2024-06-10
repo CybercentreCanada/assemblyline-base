7bbf0dea0f7dd710a8ca17f97d8e9f58e743f2a4
cccs-kevin <kevin.hardy-cooper@cyber.gc.ca>
2024-06-10 19:55:23 +0000

    Black formatting

--- assemblyline/common/identify.py
+++ assemblyline/common/identify.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import os
 import re
 import struct
 import subprocess
@@ -9,13 +10,14 @@ import uuid
 import zipfile
 from binascii import hexlify
 from typing import Dict, Optional, Tuple, Union
+from urllib.parse import unquote, urlparse
 
-import os
 import magic
 import msoffcrypto
 import yaml
 import yara
-from assemblyline.common.digests import get_digests_for_file, DEFAULT_BLOCKSIZE
+
+from assemblyline.common.digests import DEFAULT_BLOCKSIZE, get_digests_for_file
 from assemblyline.common.forge import get_cachestore, get_config, get_constants, get_datastore
 from assemblyline.common.identify_defaults import OLE_CLSID_GUIDs
 from assemblyline.common.identify_defaults import magic_patterns as default_magic_patterns
@@ -24,7 +26,6 @@ from assemblyline.common.str_utils import dotdump, safe_str
 from assemblyline.filestore import FileStoreException
 from assemblyline.remote.datatypes.events import EventWatcher
 from cart import get_metadata_only
-from urllib.parse import unquote, urlparse
 
 constants = get_constants()
 
@@ -36,15 +37,15 @@ CUSTOM_BATCH_ID = b"REM Batch extracted by Assemblyline\n"
 CUSTOM_URI_ID = "# Assemblyline URI file\n"
 
 
-class Identify():
+class Identify:
     def __init__(self, use_cache: bool = True, config=None, datastore=None, log=None) -> None:
-        self.log = log or logging.getLogger('assemblyline.identify')
+        self.log = log or logging.getLogger("assemblyline.identify")
         self.config = None
         self.datastore = None
         self.use_cache = use_cache
         self.custom = re.compile(r"^custom: ", re.IGNORECASE)
         self.lock = threading.Lock()
-        self.yara_default_externals = {'mime': '', 'magic': '', 'type': ''}
+        self.yara_default_externals = {"mime": "", "magic": "", "type": ""}
 
         # If cache is use, load the config and datastore objects to load potential items from cache
         if self.use_cache:
@@ -61,13 +62,13 @@ class Identify():
         # Register hot reloader
         if self.use_cache:
             self.reload_map = {
-                'magic': self._load_magic_file,
-                'mimes': self._load_trusted_mimes,
-                'patterns': self._load_magic_patterns,
-                'yara': self._load_yara_file
+                "magic": self._load_magic_file,
+                "mimes": self._load_trusted_mimes,
+                "patterns": self._load_magic_patterns,
+                "yara": self._load_yara_file,
             }
             self.reload_watcher: Optional[EventWatcher[str]] = EventWatcher()
-            self.reload_watcher.register('system.identify', self._handle_reload_event)
+            self.reload_watcher.register("system.identify", self._handle_reload_event)
             self.reload_watcher.start()
         else:
             self.reload_watcher = None
@@ -91,9 +92,9 @@ class Identify():
 
         if self.use_cache:
             self.log.info("Checking for custom magic patterns...")
-            with get_cachestore('system', config=self.config, datastore=self.datastore) as cache:
+            with get_cachestore("system", config=self.config, datastore=self.datastore) as cache:
                 try:
-                    patterns = cache.get('custom_patterns')
+                    patterns = cache.get("custom_patterns")
                     if patterns:
                         self.magic_patterns = yaml.safe_load(patterns)
                         self.log.info("Custom magic patterns loaded!")
@@ -102,7 +103,7 @@ class Identify():
                 except FileStoreException:
                     self.log.info("No custom magic patterns found.")
 
-        compiled_patterns = [[x['al_type'], re.compile(x['regex'], re.IGNORECASE)] for x in self.magic_patterns]
+        compiled_patterns = [[x["al_type"], re.compile(x["regex"], re.IGNORECASE)] for x in self.magic_patterns]
         with self.lock:
             self.compiled_magic_patterns = compiled_patterns
 
@@ -111,9 +112,9 @@ class Identify():
 
         if self.use_cache:
             self.log.info("Checking for custom trusted mimes...")
-            with get_cachestore('system', config=self.config, datastore=self.datastore) as cache:
+            with get_cachestore("system", config=self.config, datastore=self.datastore) as cache:
                 try:
-                    mimes = cache.get('custom_mimes')
+                    mimes = cache.get("custom_mimes")
                     if mimes:
                         trusted_mimes = yaml.safe_load(mimes)
                         self.log.info("Custom trusted mimes loaded!")
@@ -126,15 +127,15 @@ class Identify():
             self.trusted_mimes = trusted_mimes
 
     def _load_magic_file(self):
-        self.magic_file = ':'.join((constants.MAGIC_RULE_PATH, '/usr/share/file/magic.mgc'))
+        self.magic_file = ":".join((constants.MAGIC_RULE_PATH, "/usr/share/file/magic.mgc"))
 
         if self.use_cache:
             self.log.info("Checking for custom magic file...")
-            with get_cachestore('system', config=self.config, datastore=self.datastore) as cache:
+            with get_cachestore("system", config=self.config, datastore=self.datastore) as cache:
                 try:
                     custom_magic = "/tmp/custom.magic"
-                    cache.download('custom_magic', custom_magic)
-                    self.magic_file = ':'.join((custom_magic, '/usr/share/file/magic.mgc'))
+                    cache.download("custom_magic", custom_magic)
+                    self.magic_file = ":".join((custom_magic, "/usr/share/file/magic.mgc"))
                     self.log.info("Custom magic file loaded!")
                 except FileStoreException:
                     self.log.info("No custom magic file found.")
@@ -151,10 +152,10 @@ class Identify():
 
         if self.use_cache:
             self.log.info("Checking for custom yara file...")
-            with get_cachestore('system', config=self.config, datastore=self.datastore) as cache:
+            with get_cachestore("system", config=self.config, datastore=self.datastore) as cache:
                 try:
                     custom_yara = "/tmp/custom.yara"
-                    cache.download('custom_yara', custom_yara)
+                    cache.download("custom_yara", custom_yara)
                     self.yara_file = custom_yara
                     self.log.info("Custom yara file loaded!")
                 except FileStoreException:
@@ -264,7 +265,7 @@ class Identify():
                 for label in labels:
                     for entry in compiled_magic_patterns:
                         if entry[1].search(dotdump(label)):  # pylint: disable=E1101
-                            data['type'] = entry[0]
+                            data["type"] = entry[0]
                             found = True
                             break
                     if found:
@@ -276,28 +277,28 @@ class Identify():
 
         # If mime is text/* and type is unknown, set text/plain to trigger
         # language detection later.
-        if data["type"] == "unknown" and data['mime'] is not None and data['mime'].startswith("text/"):
+        if data["type"] == "unknown" and data["mime"] is not None and data["mime"].startswith("text/"):
             data["type"] = "text/plain"
 
         # Lookup office documents by GUID if we're still not sure what they are
         if data["type"] == "document/office/unknown":
             # noinspection PyBroadException
             try:
-                root_entry_property_offset = buf.find(u"Root Entry".encode("utf-16-le"))
+                root_entry_property_offset = buf.find("Root Entry".encode("utf-16-le"))
                 if -1 != root_entry_property_offset:
                     # Get root entry's GUID and try to guess document type
                     clsid_offset = root_entry_property_offset + 0x50
                     if len(buf) >= clsid_offset + 16:
-                        clsid = buf[clsid_offset: clsid_offset + 16]
+                        clsid = buf[clsid_offset : clsid_offset + 16]
                         if len(clsid) == 16 and clsid != b"\0" * len(clsid):
                             clsid_str = uuid.UUID(bytes_le=clsid)
                             clsid_str = clsid_str.urn.rsplit(":", 1)[-1].upper()
                             if clsid_str in OLE_CLSID_GUIDs:
                                 data["type"] = OLE_CLSID_GUIDs[clsid_str]
                         else:
-                            bup_details_offset = buf[
-                                : root_entry_property_offset + 0x100
-                            ].find(u"Details".encode("utf-16-le"))
+                            bup_details_offset = buf[: root_entry_property_offset + 0x100].find(
+                                "Details".encode("utf-16-le")
+                            )
                             if -1 != bup_details_offset:
                                 data["type"] = "quarantine/mcafee"
             except Exception:
@@ -311,7 +312,7 @@ class Identify():
             with self.lock:
                 yara_rules = self.yara_rules
             matches = yara_rules.match(path, externals=externals, fast=True)
-            matches.sort(key=lambda x: x.meta.get('score', 0), reverse=True)
+            matches.sort(key=lambda x: x.meta.get("score", 0), reverse=True)
             for match in matches:
                 ftype = match.meta.get("type", None)
                 if ftype:
@@ -334,7 +335,7 @@ class Identify():
                 calculate_entropy=calculate_entropy,
             )
         else:
-            with open(path, 'rb') as f:
+            with open(path, "rb") as f:
                 first_block = f.read(DEFAULT_BLOCKSIZE)
             data = self.ident(first_block, len(first_block), path)
             data["size"] = os.path.getsize(path)
@@ -539,7 +540,7 @@ def dos_ident(path: str) -> str:
 
 def uri_ident(path: str, info: Dict) -> str:
     try:
-        with open(path, 'r') as f:
+        with open(path, "r") as f:
             data = yaml.safe_load(f)
     except Exception:
         return "text/plain"
