3d07e45ffbdd149a88604de8ec44e532659313bc
Oliver Ciappara <ociappara@microsoft.com>
2024-06-26 17:04:44 +0100

    Fixed PR.

--- assemblyline/odm/models/config.py
+++ assemblyline/odm/models/config.py
@@ -208,7 +208,7 @@ class OAuthProvider(odm.Model):
     auto_no_secret: bool = odm.Boolean(default=False, description="Should we use the client secret for the OAuth?")
     client_scope: str = odm.Optional(odm.Keyword(),
                                     description="Managed Identity scope to authenticate to the OAuth provider")
-    client_tenent: List[str] = odm.List(odm.Keyword(), default=[],
+    client_tenents: List[str] = odm.List(odm.Keyword(), default=[],
                                     description="List of tenants that are allowed to authenticate to the OAuth provider")
     redirect_uri: str = odm.Optional(odm.Keyword(),
                                     description="URI to redirect to after authentication with OAuth provider")
