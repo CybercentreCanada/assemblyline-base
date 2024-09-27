2bf4381db8059face9687ac33b897dc101bb1903
cccs-eric <eric.ladouceur@cyber.gc.ca>
2024-09-27 08:53:20 -0400

    Update to Signature, Statistics and Submission ODM documentation

--- assemblyline/odm/models/signature.py
+++ assemblyline/odm/models/signature.py
@@ -11,18 +11,23 @@ STALE_STATUSES = ['INVALID']
 RULE_STATUSES = DEPLOYED_STATUSES + DRAFT_STATUSES + STALE_STATUSES
 
 
-@odm.model(index=True, store=True)
+@odm.model(index=True, store=True, description="""The Signature model within Assemblyline serves as a central framework for defining and managing security signatures, crucial components for the detection and analysis of malware. It provides a structured format that encompasses essential attributes such as a signature's name, classification level, source, and unique identifier. Moreover, it incorporates revision history, last modification timestamps, and statistical data to gauge performance and utility.
+
+The model presents users with a mix of static and dynamic information, ranging from immutable identification details to variable metadata reflecting the signature's current operational status and history of changes. This latter aspect is captured through fields that log the date and user associated with the last status update, offering a view into the signature's lifecycle.
+
+Understanding the Signature model is vital for cybersecurity professionals who are tasked with crafting precise Lucene-based search queries in Assemblyline. Mastery of this model's components will enable users to efficiently search, filter, and analyze signatures based on various parameters, thereby facilitating effective management and deployment in a cybersecurity context.
+""")
 class Signature(odm.Model):
-    classification = odm.Classification(store=True, default=Classification.UNRESTRICTED)
-    data = odm.Text(index=False, store=False)
-    last_modified = odm.Date(default="NOW")
-    name = odm.Keyword(copyto="__text__")
-    order = odm.Integer(default=1, store=False)
-    revision = odm.Keyword(default="1")
-    signature_id = odm.Optional(odm.Keyword())
-    source = odm.Keyword()
-    state_change_date = odm.Optional(odm.Date(store=False))
-    state_change_user = odm.Optional(odm.Keyword(store=False))
-    stats = odm.Compound(Statistics, default={})
-    status = odm.Enum(values=RULE_STATUSES, copyto="__text__")
-    type = odm.Keyword(copyto="__text__")
+    classification = odm.Classification(store=True, default=Classification.UNRESTRICTED, description="Indicates the sensitivity level of the signature, which dictates who can access it based on their clearance.")
+    data = odm.Text(index=False, store=False, description="Stores the actual signature data or pattern used for malware detection.")
+    last_modified = odm.Date(default="NOW", description="Records the timestamp of the most recent update to the signature. Defaults to the current time when the signature is modified.")
+    name = odm.Keyword(copyto="__text__", description="A unique and descriptive name for the signature.")
+    order = odm.Integer(default=1, store=False, description="**TODO**:Lower number means higher priority?  **Generated**:Determines the processing order of the signature relative to others. A lower number indicates higher priority.")
+    revision = odm.Keyword(default="1", description="Tracks the version of the signature, with the default starting value set to \"1\".")
+    signature_id = odm.Optional(odm.Keyword(), description="A unique identifier for the signature, which can be used for tracking and referencing purposes.")
+    source = odm.Keyword(description="Identifies the origin or the entity that provided the signature.")
+    state_change_date = odm.Optional(odm.Date(store=False), description="Captures the date when the signature's status was last updated. ")
+    state_change_user = odm.Optional(odm.Keyword(store=False), description="Records the username of the individual who last modified the signature's status.")
+    stats = odm.Compound(Statistics, default={}, description="Holds various statistical data related to the signature's performance and usage")
+    status = odm.Enum(values=RULE_STATUSES, copyto="__text__", description="Reflects the operational state of the signature, indicating whether it is deployed, in testing, or otherwise.")
+    type = odm.Keyword(copyto="__text__", description="Specifies the category or classification of the signature, which can be used for organizing and filtering signatures.")
--- assemblyline/odm/models/statistics.py
+++ assemblyline/odm/models/statistics.py
@@ -1,12 +1,17 @@
 from assemblyline import odm
 
 
-@odm.model(index=True, store=True, description="Statistics Model")
+@odm.model(index=True, store=True, description="""The Statistics model within Assemblyline is a framework that gathers and organizes quantitative data from malware analysis. It offers users essential statistical information such as counts, minimum and maximum values, averages, and sums. These data points help to quantify different attributes of the analyzed events or items.
+
+In addition, the model includes fields that track the timing of events, such as when a particular signature was first seen or most recently seen. This information can be beneficial for tracking trends and patterns over time.
+
+By utilizing the Statistics model, users are equipped with the necessary data to perform searches and analyze malware using Lucene queries. This can aid in the efficient identification and examination of cyber threats.
+""")
 class Statistics(odm.Model):
-    count = odm.Integer(default=0, description="Count of statistical hits")
-    min = odm.Integer(default=0, description="Minimum value of all stastical hits")
-    max = odm.Integer(default=0, description="Maximum value of all stastical hits")
-    avg = odm.Integer(default=0, description="Average of all stastical hits")
-    sum = odm.Integer(default=0, description="Sum of all stastical hits")
-    first_hit = odm.Optional(odm.Date(), description="Date of first hit of statistic")
-    last_hit = odm.Optional(odm.Date(), description="Date of last hit of statistic")
+    count = odm.Integer(default=0, description="Total number of times a particular data point or event has been recorded.")
+    min = odm.Integer(default=0, description="The smallest numerical value recorded among all statistical events.")
+    max = odm.Integer(default=0, description="The largest numerical value recorded among all statistical events.")
+    avg = odm.Integer(default=0, description="The mean value derived from the sum of all recorded statistical events divided by the count.")
+    sum = odm.Integer(default=0, description="The aggregate total of all numerical values from the statistical events.")
+    first_hit = odm.Optional(odm.Date(), description="The date when the first recorded instance of the statistical event occurred.")
+    last_hit = odm.Optional(odm.Date(), description="The date when the most recent instance of the statistical event was recorded.")
