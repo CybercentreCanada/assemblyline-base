a54d06f778b47e8f61e82fad9a40357ae0258569
Steve Garon <stevegaron@microsoft.com>
2024-10-15 14:59:08 +0000

    remove unused variables and fix params variables

--- assemblyline/odm/models/config.py
+++ assemblyline/odm/models/config.py
@@ -210,11 +210,11 @@ class OAuthProvider(odm.Model):
     redirect_uri: str = odm.Optional(odm.Keyword(),
                                      description="URI to redirect to after authentication with OAuth provider")
     request_token_url: str = odm.Optional(odm.Keyword(), description="URL to request token")
-    request_token_params: str = odm.Optional(odm.Keyword(), description="Parameters to request token")
+    request_token_params: Dict[str, str] = odm.Optional(odm.Mapping(odm.Keyword()),description="Parameters to request token")
     access_token_url: str = odm.Optional(odm.Keyword(), description="URL to get access token")
-    access_token_params: str = odm.Optional(odm.Keyword(), description="Parameters to get access token")
+    access_token_params: Dict[str, str] = odm.Optional(odm.Mapping(odm.Keyword()), description="Parameters to get access token")
     authorize_url: str = odm.Optional(odm.Keyword(), description="URL used to authorize access to a resource")
-    authorize_params: str = odm.Optional(odm.Keyword(), description="Parameters used to authorize access to a resource")
+    authorize_params: Dict[str, str] = odm.Optional(odm.Mapping(odm.Keyword()),description="Parameters used to authorize access to a resource")
     api_base_url: str = odm.Optional(odm.Keyword(), description="Base URL for downloading the user's and groups info")
     client_kwargs: Dict[str, str] = odm.Optional(odm.Mapping(odm.Keyword()),
                                                  description="Keyword arguments passed to the different URLs")
@@ -239,18 +239,9 @@ class OAuthProvider(odm.Model):
     username_field: str = odm.Keyword(default='uname', description="Name of the field that will contain the username")
     validate_token_with_secret: bool = odm.Boolean(
         default=False, description="Should we send the client secret while validating the access token?")
-    aad_credentials_scope: str = odm.Optional(
-        odm.Keyword(),
-        description="Scope used for authentication using Azure AD Credentials")
-    aad_wic_tenant_id: str = odm.Optional(
-        odm.Keyword(),
-        description="Tenant ID for authentication using Azure AD Workflow Identity Credentials")
     aad_wic_token_file_path: str = odm.Optional(
         odm.Keyword(),
         description="Path to the token file for authentication using Azure AD Workflow Identity Credentials")
-    aad_wic_additionally_allowed_tenants: str = odm.Optional(
-        odm.List(odm.Keyword()),
-        description="List or other tenant IDs allowed to use the Azure AD Workflow Identity Credentials token")
 
 
 DEFAULT_OAUTH_PROVIDER_AZURE = {
