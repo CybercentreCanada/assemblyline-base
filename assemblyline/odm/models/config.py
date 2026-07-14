8af28f5601fece33d7c5adacca44baad7903fa87
Adam Douglass <Adam.Douglass@cyber.gc.ca>
2026-07-14 19:30:48 +0000

    add a maximum number of pending containers

--- assemblyline/odm/models/config.py
+++ assemblyline/odm/models/config.py
@@ -882,6 +882,7 @@ class Scaler(odm.Model):
         default=False,
         description="Launch all containers in compliance with the 'Restricted' pod security standard.",
     )
+    max_pending = odm.integer(description="Maximum number of containers requested but not yet acknowledged.")
 
 
 DEFAULT_SCALER = {
@@ -908,6 +909,7 @@ DEFAULT_SCALER = {
     #     'field': [],
     #     'label': [],
     # },
+    'max_pending': 10,
 }
 
 
